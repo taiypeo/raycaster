57a1ab472364e9b9b9c9873575b7de3b5b3155da
taiypeo <Qwertygid@gmail.com>
2023-04-27 16:45:32 +0300

    Add a basic vector struct

---
+++ raycaster/vector.cpp
@@ -0,0 +1,58 @@
+#include <ostream>
+
+#include "vector.hpp"
+
+Vector::Vector(double x, double y) : x(x), y(y) {}
+
+Vector &Vector::operator+=(const Vector &other)
+{
+    x += other.x;
+    y += other.y;
+    return *this;
+}
+
+Vector &Vector::operator-=(const Vector &other)
+{
+    x -= other.x;
+    y -= other.y;
+    return *this;
+}
+
+Vector &Vector::operator*=(double constant)
+{
+    x *= constant;
+    y *= constant;
+    return *this;
+}
+
+Vector operator+(const Vector &vec1, const Vector &vec2)
+{
+    Vector result(vec1);
+    result += vec2;
+    return result;
+}
+
+Vector operator-(const Vector &vec1, const Vector &vec2)
+{
+    Vector result(vec1);
+    result -= vec2;
+    return result;
+}
+
+Vector operator*(const Vector &vec, double constant)
+{
+    Vector result(vec);
+    result *= constant;
+    return result;
+}
+
+Vector operator*(double constant, const Vector &vec)
+{
+    return vec * constant;
+}
+
+std::ostream &operator<<(std::ostream &out, const Vector &vec)
+{
+    out << "Vec(" << vec.x << ", " << vec.y << ")";
+    return out;
+}
---
+++ raycaster/vector.hpp
@@ -0,0 +1,23 @@
+#ifndef _VECTOR_HPP
+#define _VECTOR_HPP
+
+#include <ostream>
+
+struct Vector
+{
+    double x, y;
+
+    Vector(double x, double y);
+    Vector &operator+=(const Vector &other);
+    Vector &operator-=(const Vector &other);
+    Vector &operator*=(double constant);
+};
+
+Vector operator+(const Vector &vec1, const Vector &vec2);
+Vector operator-(const Vector &vec1, const Vector &vec2);
+Vector operator*(const Vector &vec, double constant);
+Vector operator*(double constant, const Vector &vec);
+
+std::ostream &operator<<(std::ostream &out, const Vector &vec);
+
+#endif
